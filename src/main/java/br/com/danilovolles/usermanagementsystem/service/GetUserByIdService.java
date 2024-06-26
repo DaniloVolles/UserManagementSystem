a6bbf6e69925bb15de48d7c5c521029bee2ba7a9
danilo.volles <danilo.volles@outlook.com>
2024-06-25 22:17:05 -0300

    rfct: get user by id -> model mapper

--- src/main/java/br/com/danilovolles/usermanagementsystem/service/GetUserByIdService.java
+++ src/main/java/br/com/danilovolles/usermanagementsystem/service/GetUserByIdService.java
@@ -3,6 +3,7 @@ package br.com.danilovolles.usermanagementsystem.service;
 import br.com.danilovolles.usermanagementsystem.dto.UserDTO;
 import br.com.danilovolles.usermanagementsystem.entity.user.User;
 import br.com.danilovolles.usermanagementsystem.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,21 +14,13 @@ public class GetUserByIdService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Optional<UserDTO> execute(Long id){
         Optional<User> user = userRepository.findById(id);
         if (user.isPresent()) {
-
-            UserDTO userDTO = new UserDTO();
-
-            userDTO.setName(user.get().getName());
-            userDTO.setEmail(user.get().getEmail());
-            userDTO.setUsername(user.get().getUsername());
-            userDTO.setRole(user.get().getRole());
-            userDTO.setCreationDate(user.get().getCreationDate());
-            userDTO.setStatus(user.get().getStatus());
-
-            return Optional.of(userDTO);
+            return Optional.of(modelMapper.map(user.get(), UserDTO.class));
         }
         return Optional.empty();
     }
