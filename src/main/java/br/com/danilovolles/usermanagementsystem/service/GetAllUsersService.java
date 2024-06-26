02e69f4d2e7666c5781f2feae34008f740a588e3
danilo.volles <danilo.volles@outlook.com>
2024-06-25 22:14:24 -0300

    rfct: get all users -> model mapper

--- src/main/java/br/com/danilovolles/usermanagementsystem/service/GetAllUsersService.java
+++ src/main/java/br/com/danilovolles/usermanagementsystem/service/GetAllUsersService.java
@@ -3,6 +3,7 @@ package br.com.danilovolles.usermanagementsystem.service;
 import br.com.danilovolles.usermanagementsystem.dto.UserDTO;
 import br.com.danilovolles.usermanagementsystem.entity.user.User;
 import br.com.danilovolles.usermanagementsystem.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,22 +15,15 @@ public class GetAllUsersService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public List<UserDTO> execute() {
         List<User> users = userRepository.findAll();
         List<UserDTO> userDTOs = new ArrayList<>();
 
         users.forEach(user -> {
-
-            UserDTO userDTO = new UserDTO();
-
-            userDTO.setName(user.getName());
-            userDTO.setUsername(user.getUsername());
-            userDTO.setEmail(user.getEmail());
-            userDTO.setRole(user.getRole());
-            userDTO.setCreationDate(user.getCreationDate());
-            userDTO.setStatus(user.getStatus());
-
+            UserDTO userDTO = modelMapper.map(user, UserDTO.class);
             userDTOs.add(userDTO);
         });
         return userDTOs;
