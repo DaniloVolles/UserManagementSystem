d8d79f4966b23efd5af7b416a21ea50634a3caf7
danilo.volles <danilo.volles@outlook.com>
2024-06-25 22:11:28 -0300

    rfct: create user -> model mapper

--- src/main/java/br/com/danilovolles/usermanagementsystem/service/CreateUserService.java
+++ src/main/java/br/com/danilovolles/usermanagementsystem/service/CreateUserService.java
@@ -3,6 +3,7 @@ package br.com.danilovolles.usermanagementsystem.service;
 import br.com.danilovolles.usermanagementsystem.dto.UserDTO;
 import br.com.danilovolles.usermanagementsystem.entity.user.User;
 import br.com.danilovolles.usermanagementsystem.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +14,22 @@ import java.util.Optional;
 public class CreateUserService {
 
     @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
     private UserRepository userRepository;
 
     public Optional<UserDTO> execute(UserDTO userData){
-        User user = new User(userData);
+
+        User user = modelMapper.map(userData, User.class);
+
         Date currentDate = new Date();
         user.setCreationDate(currentDate);
+
         userRepository.saveAndFlush(user);
-        return Optional.of(userData);
+
+        UserDTO data = modelMapper.map(user, UserDTO.class);
+
+        return Optional.of(data);
     }
 }
